b36b9c115aff20560a4e9254ab0c8990ec5c7ece
Akash-kr-gupta <ak01gupta8235@gmail.com>
2026-03-29 10:41:15 +0530

    Updated project (React + Flask full setup)

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // remove index.css import
+import reportWebVitals from "./reportWebVitals";
+
+import "./style.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Optional: performance monitoring
+reportWebVitals();
